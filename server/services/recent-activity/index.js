f1fea5e236fd79d6198444f9d2a238ede1bcfb92
Nikola <nikola.talevski@hotmail.com>
2024-02-14 04:00:32 +0100

    commit

--- server/services/recent-activity/index.js
+++ server/services/recent-activity/index.js
@@ -2,17 +2,13 @@ const config = require("../../pkg/config");
 require("../../pkg/db");
 
 const express = require("express");
-const bodyParser = require("body-parser");
 
 const api = express();
-api.use(bodyParser.json());
 
 const recentActivity = [];
 
 api.post("/api/v1/recent-activity", (req, res) => {
-  const { message } = req.body;
-  const timestamp = new Date().toISOString();
-  const activity = { timestamp, message };
+  const activity = req.body;
   recentActivity.unshift(activity);
   res.status(201).send("Activity added successfully");
 });
